7305c43ab1898bf9a2e74d92d70b373646d7e002
Marco Castillo <marcocastillo2506@gmail.com>
2018-02-15 15:42:31 -0600

    Added database and mongojs

--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -1,29 +1,18 @@
 var express = require ('express');
 var app = express();
+var mongojs = require ('mongojs');
+var db = mongojs('contactlist',['contactlist']);
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/contactlist', function(req, res) {
   console.log("i recived a get request")
-  person1 = {
-    name: "Marco",
-    email:"marcocastillo2506@gmail.com",
-    number: 5513025563
-  };
 
-  person2 = {
-    name:"Edgar",
-    email:"edgargonzales1702@gmail.com",
-    number: 5516035163
-  };
+db.contactlist.find(function(err, docs){
+  console.log(docs);
+  res.json(docs);
+})
 
-  person3 = {
-    name:"Alexis",
-    email:"alexisgonzales0101@gmail.com",
-    number: 5558971521
-  };
-  var contactlist = [person1, person2, person3];
-  res.json(contactlist)
 });
 
 app.listen(3000);
