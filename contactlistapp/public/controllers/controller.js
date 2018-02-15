64f3d9cf942946c0b943f3d5030827b4f1d3be72
Marco Castillo <marcocastillo2506@gmail.com>
2018-02-15 08:47:41 -0600

    Created dummydata for table

--- contactlistapp/public/controllers/controller.js
+++ contactlistapp/public/controllers/controller.js
@@ -1,4 +1,25 @@
 var app = angular.module("myApp",[]);
 app.controller("AppCtrl",function($scope){
   console.log("Testing controller");
+
+  person1 = {
+    name: "Marco",
+    email:"marcocastillo2506@gmail.com",
+    number: 5513025563
+  };
+
+  person2 = {
+    name:"Edgar",
+    email:"edgargonzales1702@gmail.com",
+    number: 5516035163
+  };
+
+  person3 = {
+    name:"Alexis",
+    email:"alexisgonzales0101",
+    number: 5558971521
+  };
+
+  var contactlist = [person1, person2, person3];
+
 })
