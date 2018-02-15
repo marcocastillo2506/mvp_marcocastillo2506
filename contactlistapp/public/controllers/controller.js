8933038a9908d5cf8f2e916c52800cab8cf20032
Marco Castillo <marcocastillo2506@gmail.com>
2018-02-15 13:56:12 -0600

    Made get request

--- contactlistapp/public/controllers/controller.js
+++ contactlistapp/public/controllers/controller.js
@@ -1,37 +1,20 @@
 
 
 var myApp = angular.module("myApp",[]);
-myApp.controller('AppCtrl',['$scope','$http',function($scope,$http){
+myApp.controller('AppCtrl',function($scope,$http){
   console.log("Testing controller");
+
 
   $http({
     method:'GET',
-    url:'/'
+    url:'contactlist'
   }).then(function successCallback(response){
     console.log("I got the data i requested");
-    //$scope.contactlist = response.data;
+    $scope.contactlist = response.data;
   },function errorCallback(response){
     //content error
   });
 
-  person1 = {
-    name: "Marco",
-    email:"marcocastillo2506@gmail.com",
-    number: 5513025563
-  };
 
-  person2 = {
-    name:"Edgar",
-    email:"edgargonzales1702@gmail.com",
-    number: 5516035163
-  };
 
-  person3 = {
-    name:"Alexis",
-    email:"alexisgonzales0101@gmail.com",
-    number: 5558971521
-  };
-  var contactlist = [person1, person2, person3];
-  $scope.contactlist = contactlist;
-
-}]);
+});
