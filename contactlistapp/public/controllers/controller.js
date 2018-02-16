602e13e44bdf8541162d0bf85eb1a6ca35ee71f2
Marco Castillo <marcocastillo2506@gmail.com>
2018-02-15 19:02:28 -0600

    Added remove botton , also got it to work

--- contactlistapp/public/controllers/controller.js
+++ contactlistapp/public/controllers/controller.js
@@ -14,10 +14,44 @@ myApp.controller('AppCtrl',function($scope,$http){
   },function errorCallback(response){
     //content error
   });
+   var refresh = function(){
+     $http({
+       method:'GET',
+       url:'contactlist'
+     }).then(function successCallback(response){
+       console.log("I got the data i requested");
+       $scope.contactlist = response.data;
+     },function errorCallback(response){
+       //content error
+     });
+   }
 
-$scope.addContact =  function(){
-  console.log($scope.contact);
-  $http.post('/contactlist', $scope.contact);
-}
+  $scope.addContact = function() {
+      console.log($scope.contact);
+      $http({
+            method: 'POST',
+            url: '/contactlist',
+            data: $scope.contact
+          })
+          .then(function(response) {
+            $scope.contact = ""; //Clear input box
+            console.log('POST Response: '+ response.statusText);
+            refresh();
+      });
+    };﻿
 
+    $scope.remove = function(id){
+      console.log(id);
+    //$http.delete('/contactlist/' + id,id)
+    $http({
+      method:'DELETE',
+      url: '/contactlist/'+ id,id
+    }).then(function successCallback(response){
+      console.log("DELETED");
+      $scope.contactlist = response.data;
+    },function errorCallback(response){
+      //content error
+    });
+      refresh();
+    };
 });
